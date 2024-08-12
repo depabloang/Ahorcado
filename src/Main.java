03170f6204845b9b22017f53e78ef31240ca28da
angel <depabloruizangel@gmail.com>
2024-08-12 11:57:01 +0200

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hola mundo");
+		StartApp();
+		
+
+	}
+	
+	public static void StartApp() {
+		final String[] FASE_1 = {
+			"----------"
+		};
+		final String[] FASE_2 = {
+				
+		};
+		final String[] FASE_3;
+		final String[] FASE_4;
+		final String[] FASE_5;
+		final String[] FASE_6;
+		final String[] FASE_FINAL = {
+				"_________",
+				"|       |",
+				"|       o",
+				"|      \\|/",
+				"|      / \\",
+				"|",
+				"----------"
+		};
+		
+		
+		
+	}
+}
