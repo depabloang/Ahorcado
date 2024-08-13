a611a7df24af78fef21df5456c8cca466c3804b1
mondo <depabloangel@protonmail.com>
2024-08-13 22:38:39 +0200

    Creadas clases App, Vista y Lógica

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,67 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hola mundo");
 		StartApp();
 		
 
-	}
+	}//Crear tres clases, Lógica, Vista y App. App tiene dos instancias de cada una
 	
 	public static void StartApp() {
+		
+		final String palabra = "Casa";
+		final char[] opciones = palabra.toUpperCase().toCharArray();
+		for(int i = 0; i < opciones.length; i++) {
+			System.out.format("_ ");
+		}
+		
 		final String[] FASE_1 = {
 			"----------"
 		};
 		final String[] FASE_2 = {
-				
+				"",
+				"|",
+				"|",
+				"|",
+				"|",
+				"|",
+				"----------"
 		};
-		final String[] FASE_3;
-		final String[] FASE_4;
-		final String[] FASE_5;
-		final String[] FASE_6;
+		final String[] FASE_3 = {
+				"_________",
+				"|",
+				"|",
+				"|",
+				"|",
+				"|",
+				"----------"
+		};
+		final String[] FASE_4 = {
+				"_________",
+				"|       |",
+				"|",
+				"|",
+				"|",
+				"|",
+				"----------"
+		};
+		final String[] FASE_5 = {
+				"_________",
+				"|       |",
+				"|       o",
+				"|",
+				"|",
+				"|",
+				"----------"
+		};
+		final String[] FASE_6 = {
+				"_________",
+				"|       |",
+				"|       o",
+				"|      \\|/",
+				"|",
+				"|",
+				"----------"
+		};
 		final String[] FASE_FINAL = {
 				"_________",
 				"|       |",
@@ -28,8 +72,6 @@ public class Main {
 				"|",
 				"----------"
 		};
-		
-		
 		
 	}
 }
